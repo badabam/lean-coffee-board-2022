d4023d404563b76d81323a8b79582f5798232e6b
Tim Petersen <tim.petersen1@outlook.de>
2021-05-28 15:12:35 +0200

    Add Error routes

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express()
 app.use('/', express.json())
 app.use('/api/users', require('./routes/users'))
 app.use('/api/cards', require('./routes/cards'))
-//app.use(require('./routes/error'))
+
+// catch all (404)
+app.use((req, res) => res.sendStatus(404))
+
+app.use(require('./routes/error'))
 
 app.listen(4000, () => {
   console.log(`Server started at http://localhost:4000`)
