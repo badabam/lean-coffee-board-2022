e8477e8e9006decd90e42f1ccba4941b69221d55
Jeremias Erbs <jerry@badabam.de>
2021-06-04 11:34:00 +0200

    Add environment variables

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 
+require('dotenv').config()
+const { PORT = 4000, SPOTIFY_TOKEN } = process.env
+
 mongoose
   .connect('mongodb://localhost:27017/lean-coffee-board', {
     useNewUrlParser: true,
@@ -15,6 +18,7 @@ const app = express()
 
 // add middleware for json data
 app.use('/api', express.json()) // (req, res, next) => {...}
+app.get('/api/token', (req, res) => res.send(SPOTIFY_TOKEN))
 app.use('/api/users', require('./routes/users'))
 app.use('/api/cards', require('./routes/cards'))
 app.use(express.static('client/build'))
@@ -27,6 +31,6 @@ app.use('*', (req, res) => {
 // error route
 app.use(require('./routes/error'))
 
-app.listen(4000, () => {
-  console.log(`Server started at http://localhost:4000`)
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`)
 })
